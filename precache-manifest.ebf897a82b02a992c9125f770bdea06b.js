dcacc7206b09112a66a107b71de5bef86c5071cc
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-09-06 00:43:45 +0530

    Updates

---
+++ precache-manifest.ebf897a82b02a992c9125f770bdea06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51e1a6a1bc27974df11bf9816481087",
+    "url": "/financial-portfolio-tracker-app/index.html"
+  },
+  {
+    "revision": "3d6ee2e23c5cb6cb16cc",
+    "url": "/financial-portfolio-tracker-app/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "8e0eb8c31a7370acb1ea",
+    "url": "/financial-portfolio-tracker-app/static/css/main.eae95a22.chunk.css"
+  },
+  {
+    "revision": "3d6ee2e23c5cb6cb16cc",
+    "url": "/financial-portfolio-tracker-app/static/js/2.63947f34.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/financial-portfolio-tracker-app/static/js/2.63947f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0eb8c31a7370acb1ea",
+    "url": "/financial-portfolio-tracker-app/static/js/main.8d1aca99.chunk.js"
+  },
+  {
+    "revision": "d1411b2191516bb460d9",
+    "url": "/financial-portfolio-tracker-app/static/js/runtime-main.d39505d0.js"
+  }
+]);
